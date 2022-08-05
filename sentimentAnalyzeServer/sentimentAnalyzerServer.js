513e7ee2aa3d88658b3863cbef599f2b21cb31ff
J Albuquerque <john.albuquerque@gmail.com>
2022-08-05 01:00:45 +0000

    Fixed dotenv variables

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,6 @@
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
+require('dotenv').config();
 const express = require('express');
 const app = new express();
 
@@ -27,9 +28,9 @@ function getNLUInstance() {
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
         version: '2021-08-01',
         authenticator: new IamAuthenticator ({
-            apikey: api_key
+            apikey: process.env.API_KEY
         }),
-        serviceUrl: api_url
+        serviceUrl: process.env.API_URL
     });
     return naturalLanguageUnderstanding;
 }
